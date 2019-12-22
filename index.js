a04d49a2f24fa03c2d4f51844aa507d53cdb902f
ilyakoptev <ilya.koptev@gmail.com>
2019-12-22 18:01:07 +0200

    edit router

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-router.post('/register', (req, res) => {
-    res.send('Register')
-})
 
 
 app.listen(3000, () => console.log('Server up and running'));
