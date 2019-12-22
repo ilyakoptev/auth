5c2ca75560afd2c2b66c079d4fcf8f82a9af32a4
ilyakoptev <ilya.koptev@gmail.com>
2019-12-22 18:02:34 +0200

    import route to index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+//import routes
+const authRoute = require('./routes/auth');
+
+
 
 
 app.listen(3000, () => console.log('Server up and running'));
