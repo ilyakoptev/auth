942a3296938f4a0d74093c1718c549c5bfc46114
ilyakoptev <ilya.koptev@gmail.com>
2019-12-22 18:00:29 +0200

    add routers and empty route Register

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-
+router.post('/register', (req, res) => {
+    res.send('Register')
+})
 
 
 app.listen(3000, () => console.log('Server up and running'));
