800859ecbfdb5d4190b3028dbf29779444a054ba
ilyakoptev <ilya.koptev@gmail.com>
2019-12-24 18:03:09 +0200

    adding validation email

--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,13 @@ router.post('/register', async(req, res) => {
     } = registerValidation(req.body)
     if (error) return res.status(400).send(error.details[0].message); // return error if validation is fail 
 
+    //checking if the user is already in the database 
+    const emailExist = await User.findOne({
+        email: req.body.email
+    })
+    if (emailExist) return res.status(400).send('Email already exists');
+    // create a new user 
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
