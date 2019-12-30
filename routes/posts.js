c811ac99b7eeecb0ad74c4b414bb3c9c590a8cc0
ilyakoptev <ilya.koptev@gmail.com>
2019-12-30 12:54:47 +0200

    add verify to posts token to get access deny

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const verify = require('./verifyToken');
 
-router.get('/', (req, res) => {
+
+router.get('/', verify, (req, res) => {
     res.json({
         posts: {
             title: 'my first post',
